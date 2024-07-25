9596ef681dc52f358d04c5447149b9c9be8f25d2
wangxuan <wangxuanniu@yeah.net>
2024-07-25 09:51:03 +0800

    fix:修改头部

--- components/Heard.vue
+++ components/Heard.vue
@@ -2,14 +2,25 @@
  * @Author: wangxuan wangxuanniu@yeah.net
  * @Date: 2024-07-23 14:14:47
  * @LastEditors: wangxuan wangxuanniu@yeah.net
- * @LastEditTime: 2024-07-24 10:40:31
+ * @LastEditTime: 2024-07-24 15:53:02
  * @FilePath: /nuxt3-app/components/heard.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%
 -->
 
 <template>
-  <div class="h-10 bg-blue-500 text-white flex justify-center items-center">
-    这是头部
+  <div class="h-15 bg-white text-black flex justify-between items-center px-8 py-8">
+    <div class="">产品展示</div>
+    <div class="flex items-center gap-10">
+      <div class="mr-2">Company</div>
+      <div class="mr-2">Products</div>
+      <div class="mr-2">The Cable Lab</div>
+      <div class="mr-2">Media</div>
+      <div class="mr-2">Contact</div>
+      <UAvatar
+        src="https://avatars.githubusercontent.com/u/739984?v=4"
+        alt="Avatar"
+      />
+    </div>
   </div>
 
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,20 @@
  * @Author: wangxuan wangxuanniu@yeah.net
  * @Date: 2024-07-23 14:18:28
  * @LastEditors: wangxuan wangxuanniu@yeah.net
- * @LastEditTime: 2024-07-24 13:27:25
+ * @LastEditTime: 2024-07-24 16:54:16
  * @FilePath: /nuxt3-app/pages/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
   <div class="p-5">
-    <UInput v-model="value" @input="debounceFn(value)" />
-    <div class="pt-5">
-      <UButton @click="debounceFn(value)">按钮</UButton>
-    </div>
-    <div class="mt-5">
-      <NuxtLink to="/about">关于我门/about us</NuxtLink>
+    <div class="flex justify-center max-h-screen">
+      <div class="max-h-full">
+        <!-- <video autoplay loop muted playsinline preload="auto" id="banner-video">
+          <source src="https://cdn.syyzny.com/sites/kukacable.com/uploads/media/20240627/1-24062G43619551.mp4" type="video/mp4">
+        </video> -->
+
+        <video src="http://dev.hd.huadaocart/assets/vedio/vedio-1.mp4" width="100%" height="100%" autoplay loop muted playsinline preload="auto"></video>
+      </div>
     </div>
   </div>
 
